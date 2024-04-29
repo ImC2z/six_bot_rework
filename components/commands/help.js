160a02028f57ac6f0c1fdda7172ed37035772542
ImC2z <cyruschu0614@gmail.com>
2024-04-29 23:14:15 +1000

    array map refactor

--- components/commands/help.js
+++ components/commands/help.js
@@ -1,19 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require(`fs`);
 
-const optionChoices = [];
 const commandCategories = fs.readdirSync(`./components/commands`).filter(file => !file.endsWith(`.js`));
-for (const category of commandCategories) {
-	optionChoices.push({ name: category, value: category });
-}  
+const categoryChoices = commandCategories.map(category => {
+	return {
+		name: category,
+		value: category
+	}
+});
 
 const format = new SlashCommandBuilder()
-	.setName('help')
-	.setDescription(`Display the help menu`)
-	.addStringOption(option =>
-		option.setName('category')
-			.setDescription('Help category')
-			.setRequired(false)
-            .addChoices(...optionChoices));
+.setName('help')
+.setDescription(`Display the help menu`)
+.addStringOption(option =>
+	option.setName('category')
+	.setDescription('Help category')
+	.setRequired(false)
+	.addChoices(...categoryChoices)
+);
 
 module.exports = format
