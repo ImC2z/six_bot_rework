e11c00057887340dc466e3420b5b2b7c10e4f1eb
ImC2z <cyruschu0614@gmail.com>
2024-04-29 23:11:55 +1000

    spread op refactor

--- register_global_commands.js
+++ register_global_commands.js
@@ -4,36 +4,32 @@ const bot_key = process.env.hazelbotkey;
 const { REST } = require(`@discordjs/rest`);
 const { Routes } = require(`discord-api-types/v9`);
 
-const commands = [];
 const baseCommands = fs.readdirSync(`./components/commands`).filter(file => file.endsWith(`.js`));
-for (const commandFile of baseCommands) {
-  const command = require(`./components/commands/${commandFile}`);
-  commands.push(command.toJSON());
-}
 const commandCategories = fs.readdirSync(`./components/commands`).filter(file => !file.endsWith(`.js`));
-for (const category of commandCategories) {
-  const commandFiles = fs.readdirSync(`./components/commands/${category}`).filter(file => file !== `index.js`);
-  for (const commandFile of commandFiles) {
-    const command = require(`./components/commands/${category}/${commandFile}`);
-    commands.push(command.toJSON());
-  }
-}
 
-const rest = new REST({ version: `9` }).setToken(bot_key);
+const commands = [
+    ...baseCommands.map(commandFile => require(`./components/commands/${commandFile}`).toJSON()),
+    ...commandCategories.map(category => {
+        const commandFiles = fs.readdirSync(`./components/commands/${category}`).filter(file => file !== `index.js`);
+        return commandFiles.map(commandFile => require(`./components/commands/${category}/${commandFile}`).toJSON());
+    }).reduce((total, current) => total.concat(current))
+];
+
+const rest = new REST({ version: `10` }).setToken(bot_key);
 const CLIENT_ID = `995290849275547658`;
 const GUILD_ID = `734674029587464213`; // 69 HOEm
 
 (async () => {
-  try {
-    console.log(`Started refreshing application (/) commands.`);
+    try {
+        console.log(`Started refreshing application (/) commands.`);
 
-    await rest.put(
-      Routes.applicationCommands(CLIENT_ID),
-      { body: commands },
-    );
+        await rest.put(
+            Routes.applicationCommands(CLIENT_ID),
+            { body: commands },
+        );
 
-    console.log(`Successfully reloaded application (/) commands.`);
-  } catch (error) {
-    console.error(error);
-  }
+        console.log(`Successfully reloaded application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
 })();
